a75ba11fc2ef0f63e04c3f32b6fbc6062110354d
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-06-04 21:28:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -125,37 +125,8 @@ bot.on("message", async message => {
      }
     
     
-    module.exports = (client) => {
-    const channelId = "838185605329715253";
-    const rulesChannel = "844285176154292225";
-    client.on('guildMemberAdd', async(member) => {
-      console.log(member);
+   
 
-      const message = BerryGod üdvözöl <@${member.id}>. Érezd jól magad és tartsd be a szabályokat ${member.guild.channels.cache .get(rulesChannel).toString()} a szabályokhoz ezt a szobát csekkold le!;
-
-      const channel = member.guild.channels.cache.get(channelId);
-      channel.send(message);
-    });
-  };
-
-
-LEAVE:
-
-  module.exports = (client) => {
-    const channelId = "847171439051538472";
-    client.on('guildMemberRemove', async(member) => {
-      console.log(member);
-
-      const message = Sajnos <@${member.id}> kilépett a szerverről, remélem még találkozunk👋!;
-
-      const channel = member.guild.channels.cache.get(channelId);
-      channel.send(message);
-    });
-  };
-    
-
-    
-    
     
     
   
