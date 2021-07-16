12192078da317a0cbb073530da9e9ca66b1610b5
BerryGod <84345559+BerryGod@users.noreply.github.com>
2021-07-16 09:41:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -153,7 +153,48 @@ bot.on("message", async message => {
     }
     
 
-    
+    if(cmd === `${prefix}kutya`) {
+       let msg = await message.channel.send('Generálás... ')
+
+  var dog;
+
+  dog = await superagent
+      .get("https://random.dog/woof.json");
+
+  while (dog.body.url.endsWith(".webm") || dog.body.url.endsWith(".mp4")) {
+      dog = await superagent
+          .get("https://random.dog/woof.json");
+      console.log(dog.body)
+  }
+  msg.delete()
+  var embed = new Discord.MessageEmbed()
+      .setColor("#ff0000")
+      .setTitle("Unrealy | Kutya")
+      .setImage(dog.body.url)
+      .setFooter(`Forrás: Athox`)
+  message.channel.send(embed);
+}
+if(cmd === `${prefix}Kutya`) {
+    let msg = await message.channel.send('Generálás... ')
+
+var dog;
+
+dog = await superagent
+   .get("https://random.dog/woof.json");
+
+while (dog.body.url.endsWith(".webm") || dog.body.url.endsWith(".mp4")) {
+   dog = await superagent
+       .get("https://random.dog/woof.json");
+   console.log(dog.body)
+}
+msg.delete()
+var embed = new Discord.MessageEmbed()
+   .setColor("#ff0000")
+   .setTitle("Unrealy | Kutya")
+   .setImage(dog.body.url)
+   .setFooter(`Forrás: Athox`)
+message.channel.send(embed);
+}
   
  
     
